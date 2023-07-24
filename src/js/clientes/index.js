9dd98df594b44a4f2aee862b92e83c2e7c0d26cc
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-07-24 17:43:47 -0600

    crear funcion buscar, cancelar accion y colocar datos para el crud clientes

--- src/js/clientes/index.js
+++ src/js/clientes/index.js
@@ -1,4 +1,6 @@
+
 // Obtenemos referencias a los elementos del formulario y otros elementos del DOM
+
 const formulario = document.querySelector('form');
 const tablaClientes = document.getElementById('tablaClientes');
 const btnBuscar = document.getElementById('btnBuscar');
@@ -55,5 +57,107 @@ const guardar = async (evento) => {
   }
 };
 
-// se asiganan el eventos a los elementos (botones).
+
+
+//se crea la funcion buscar 
+const buscar = async () => {
+    let cliente_nombre = formulario.cliente_nombre.value;
+    let cliente_nit = formulario.cliente_nit.value;
+    const url = `/crud_ajax/controladores/clientes/index.php?cliente_nombre=${cliente_nombre}&cliente_nit=${cliente_nit}`;
+    const config = {
+      method: 'GET',
+    };
+  
+    try {
+      const respuesta = await fetch(url, config);
+      const data = await respuesta.json();
+  
+      tablaClientes.tBodies[0].innerHTML = '';
+      const fragment = document.createDocumentFragment();
+      console.log(data);
+      if (data.length > 0) {
+        let contador = 1;
+        data.forEach((cliente) => {
+
+          // se crean elementos 
+          const tr = document.createElement('tr');
+          const td1 = document.createElement('td');
+          const td2 = document.createElement('td');
+          const td3 = document.createElement('td');
+          const td4 = document.createElement('td');
+          const td5 = document.createElement('td');
+          const buttonModificar = document.createElement('button');
+          const buttonEliminar = document.createElement('button');
+  
+          // se agregan clases de boostrap a los botones.
+          buttonModificar.classList.add('btn', 'btn-warning');
+          buttonEliminar.classList.add('btn', 'btn-danger');
+          buttonModificar.textContent = 'Modificar';
+          buttonEliminar.textContent = 'Eliminar';
+          
+          buttonModificar.addEventListener('click', () => colocarDatos(cliente));
+          buttonEliminar.addEventListener('click', () => eliminar(cliente.CLIENTE_ID));
+  
+          td1.innerText = contador;
+          td2.innerText = cliente.CLIENTE_NOMBRE;
+          td3.innerText = cliente.CLIENTE_NIT;
+  
+          // ESTRUCTURANDO DOM
+          td4.appendChild(buttonModificar);
+          td5.appendChild(buttonEliminar);
+          tr.appendChild(td1);
+          tr.appendChild(td2);
+          tr.appendChild(td3);
+          tr.appendChild(td4);
+          tr.appendChild(td5);
+  
+          fragment.appendChild(tr);
+  
+          contador++;
+        });
+      } else {
+        const tr = document.createElement('tr');
+        const td = document.createElement('td');
+        td.innerText = 'No existen registros';
+        td.colSpan = 5;
+        tr.appendChild(td);
+        fragment.appendChild(tr);
+      }
+  
+      tablaClientes.tBodies[0].appendChild(fragment);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // funcion para colocar los datos a la hora de modificar 
+  const colocarDatos = (datos) => {
+    formulario.cliente_nombre.value = datos.CLIENTE_NOMBRE;
+    formulario.cliente_nit.value = datos.CLIENTE_NIT;
+    formulario.cliente_id.value = datos.CLIENTE_ID;
+  
+    btnGuardar.disabled = true;
+    btnGuardar.parentElement.style.display = 'none';
+    btnBuscar.disabled = true;
+    btnBuscar.parentElement.style.display = 'none';
+    btnModificar.disabled = false;
+    btnModificar.parentElement.style.display = '';
+    btnCancelar.disabled = false;
+    btnCancelar.parentElement.style.display = '';
+    divTabla.style.display = 'none';
+  };
+  //crear funcion cancelar accion para no crear otro registro de cliente
+  const cancelarAccion = () => {
+    btnGuardar.disabled = false;
+    btnGuardar.parentElement.style.display = '';
+    btnBuscar.disabled = false;
+    btnBuscar.parentElement.style.display = '';
+    btnModificar.disabled = true;
+    btnModificar.parentElement.style.display = 'none';
+    btnCancelar.disabled = true;
+    btnCancelar.parentElement.style.display = 'none';
+    divTabla.style.display = '';
+  };
+
+  // se asiganan el eventos a los elementos (botones).
 formulario.addEventListener('submit', guardar);
+btnBuscar.addEventListener('click', buscar);
